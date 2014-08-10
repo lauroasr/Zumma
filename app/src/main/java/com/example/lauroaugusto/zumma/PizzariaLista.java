961f82bebabd477b1157f33a56436e124f75ae83
Lauro <lauroasr@gmail.com>
2014-08-10 17:07:05 -0300

    Mil tretas

--- app/src/main/java/com/example/lauroaugusto/zumma/PizzariaLista.java
+++ app/src/main/java/com/example/lauroaugusto/zumma/PizzariaLista.java
@@ -1,7 +1,6 @@
 package com.example.lauroaugusto.zumma;
 
 
-import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -10,7 +9,10 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
-import com.example.lauroaugusto.zumma.R;
+import com.db4o.Db4oEmbedded;
+import com.db4o.ObjectContainer;
+import com.db4o.ObjectSet;
+import com.db4o.query.Predicate;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,9 +29,9 @@ public class PizzariaLista extends android.support.v4.app.Fragment implements Ad
         view = inflater.inflate(R.layout.fragment_pizzaria_lista,container,false);
 
         lvPizzarias = (ListView) view.findViewById(R.id.lvTarap);
-        List<Pizzaria> pizzaria = gerarPizzarias();
+        List<Pizzaria> pizzarias = buscarPizzarias();
 
-        PizzariaAdapter pizzariasAdapter = new PizzariaAdapter(getActivity(), pizzaria);
+        PizzariaAdapter pizzariasAdapter = new PizzariaAdapter(getActivity(), pizzarias);
         lvPizzarias.setAdapter(pizzariasAdapter);
         lvPizzarias.setOnItemClickListener(this);
 
@@ -37,30 +39,30 @@ public class PizzariaLista extends android.support.v4.app.Fragment implements Ad
         return view;
     }
 
-    private List<Pizzaria> gerarPizzarias(){
+    private List<Pizzaria> buscarPizzarias(){
         List<Pizzaria> pizzarias = new ArrayList();
-        pizzarias.add(criaPizzaria("Loja1", R.drawable.ic_launcher, 0));
-        pizzarias.add(criaPizzaria("Loja2", R.drawable.ic_launcher, 1));
-        pizzarias.add(criaPizzaria("Loja3", R.drawable.ic_launcher, 2));
-        pizzarias.add(criaPizzaria("Loja4", R.drawable.ic_launcher, 3));
-        pizzarias.add(criaPizzaria("Loja5", R.drawable.ic_launcher, 4));
-        pizzarias.add(criaPizzaria("Loja6", R.drawable.ic_launcher, 5));
-        pizzarias.add(criaPizzaria("Loja7", R.drawable.ic_launcher, 6));
-        pizzarias.add(criaPizzaria("Loja8", R.drawable.ic_launcher, 7));
-        pizzarias.add(criaPizzaria("Loja9", R.drawable.ic_launcher, 8));
-        pizzarias.add(criaPizzaria("Loja10", R.drawable.ic_launcher, 9));
-        pizzarias.add(criaPizzaria("Loja11", R.drawable.ic_launcher, 10));
-        pizzarias.add(criaPizzaria("Loja12", R.drawable.ic_launcher, 11));
-        pizzarias.add(criaPizzaria("Loja13", R.drawable.ic_launcher, 12));
-        pizzarias.add(criaPizzaria("Loja14", R.drawable.ic_launcher, 13));
-        pizzarias.add(criaPizzaria("Loja15", R.drawable.ic_launcher, 14));
+        ObjectContainer oc = Db4oEmbedded.openFile(getActivity().getDir("data", 0) + "/" + "db.db4o");
+        ObjectSet<Pizzaria> query = oc.query(Pizzaria.class);
 
+        if (query.size() == 0)
+        {
+            // TODO: Buscar do seridor as pizzarias.
+
+            for (int i = 0; i < 10; i++) {
+                Pizzaria pizzaria = new Pizzaria("Loja " + (i + 1), R.drawable.ic_launcher, i + 1);
+                pizzarias.add(pizzaria);
+                oc.store(pizzaria);
+            }
+            oc.commit();
+        }
+        else
+        {
+            for (Pizzaria pizzaria : query) {
+                pizzarias.add(pizzaria);
+            }
+        }
+        oc.close();
         return pizzarias;
-    }
-
-    private Pizzaria criaPizzaria(String nome, int idImg, int id){
-        Pizzaria pizzaria = new Pizzaria(nome,idImg, id);
-        return pizzaria;
     }
 
 
